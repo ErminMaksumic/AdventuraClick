fb69578565b210a397d04fdb704ec719df23da98
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2023-09-14 22:28:30 +0200

    Added base services

---
+++ AdventuraClick.Service/Interfaces/ITravelService.cs
@@ -0,0 +1,13 @@
+﻿using AdventuraClick.Model;
+using AdventuraClick.Model.SearchObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventuraClick.Service.Interfaces
+{
+    public interface ITravelService : ICRUDService<Model.Travel, TravelSearchObject, TravelUpsertRequest, TravelUpsertRequest>
+    {}
+}
