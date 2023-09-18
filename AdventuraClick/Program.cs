9680ca478c980a70e5ff59bb3ce18ffddd23fc96
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2023-09-18 03:13:28 -0700

    Updated automapper (#9)

--- AdventuraClick/Program.cs
+++ AdventuraClick/Program.cs
@@ -3,7 +3,7 @@ using AdventuraClick.Service.Database;
 using AdventuraClick.Service.Interfaces;
 using AdventuraClick.Model.SearchObjects;
 using AdventuraClick.Service.Implementation;
-using AutoMapper;
+using AdventuraClick.Service.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Services
+builder.Services.AddScoped<IBaseService<AdventuraClick.Model.Role, BaseSearchObject>, RoleService>();
 builder.Services.AddScoped<IBaseService<AdventuraClick.Model.Role, BaseSearchObject>, RoleService>();
 builder.Services.AddTransient<ITravelService, TravelService>();
 builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IReservationService, ReservationService>();
+// 
 builder.Services.AddAutoMapper(typeof(Mapper).Assembly);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
