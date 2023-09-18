a3948ef2b78f14b805ca7f9fce1a3f1284609b77
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2023-09-18 12:11:28 +0200

    Updated automapper

---
+++ AdventuraClick.Service/Mapper/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using AdventuraClick.Model.Requests;
+using AutoMapper;
+
+namespace AdventuraClick.Service.Mapper
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            // Travel
+            CreateMap<Database.Travel, Model.Travel>().ReverseMap();
+            CreateMap<Database.Travel, Model.TravelUpsertRequest>().ReverseMap();
+            // User
+            CreateMap<Database.User, Model.User>().ReverseMap();
+            CreateMap<Database.User, UserInsertRequest>().ReverseMap();
+            CreateMap<Database.User, UserUpdateRequest>().ReverseMap();
+            // Reservation
+            CreateMap<Database.Reservation, Model.Reservation>().ReverseMap();
+            CreateMap<Database.Reservation, ReservationInsertRequest>().ReverseMap();
+            CreateMap<ReservationInsertRequest, Database.Reservation>().ReverseMap();
+            CreateMap<Database.Reservation, ReservationUpdateRequest>().ReverseMap();
+            // Role
+            CreateMap<Database.Role, Model.Role>().ReverseMap();
+        }
+    }
+}
