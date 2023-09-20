974e9cd23a13bd7e88dcadaf1ffe5a6e33b85c5b
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2023-09-20 10:44:11 +0200

    Added providers and models

---
+++ adventuraclick_mobile/lib/providers/user_provider.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:adventuraclick_mobile/model/user.dart';
+import 'package:adventuraclick_mobile/providers/base_provider.dart';
+
+class UserProvider extends BaseProvider<User> {
+  UserProvider() : super("User");
+
+  @override
+  User fromJson(data) {
+    return User.fromJson(data);
+  }
+
+  Future<User> login() async {
+    var url = Uri.parse("$fullUrl/login");
+
+    Map<String, String> headers = createHeaders();
+
+    var response = await http!.get(url, headers: headers);
+
+    if (isValidResponseCode(response)) {
+      var data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      throw Exception("An error occured!");
+    }
+  }
+
+  Future<User?> register(dynamic request) async {
+    var url = Uri.parse("$fullUrl/register");
+
+    Map<String, String> headers = {"Content-Type": "application/json"};
+    var jsonRequest = jsonEncode(request);
+    var response = await http!.post(url, headers: headers, body: jsonRequest);
+
+    if (isValidResponseCode(response)) {
+      var data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      return null;
+    }
+  }
+}
