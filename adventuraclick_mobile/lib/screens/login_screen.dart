f47bf78f1a52011a988ffa06ff0a803642850185
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2023-09-20 14:24:51 +0200

    Added login page style

--- adventuraclick_mobile/lib/screens/login_screen.dart
+++ adventuraclick_mobile/lib/screens/login_screen.dart
@@ -1,66 +1,52 @@
-
-
 import 'package:adventuraclick_mobile/providers/user_provider.dart';
 import 'package:adventuraclick_mobile/utils/auth_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// ignore: must_be_immutable
 class LoginScreen extends StatelessWidget {
   static const String routeName = "/login";
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   late UserProvider _userProvider;
   final _formKey = GlobalKey<FormState>();
+
   LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //_userProvider = context.read<UserProvider>();
     _userProvider = Provider.of(context, listen: false);
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Login"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 350,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/background.jpg'),
-                        fit: BoxFit.fill)),
-                child: Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 280),
-                      child: const Center(
-                        child: Center(
-                            child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.cyan,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold),
-                        )),
-                      ),
-                    )
-                  ],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Center(
+          child:Text("Login"),
+          ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 350,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(50),
-                child: Form(
-                  key: _formKey,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: TextFormField(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      children: [
+                        TextFormField(
                           validator: (value) {
                             if (value!.isEmpty) {
                               return "Required field!";
@@ -69,18 +55,16 @@ class LoginScreen extends StatelessWidget {
                           },
                           controller: _usernameController,
                           decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Username",
-                              hintStyle: TextStyle(color: Colors.cyan)),
+                            prefixIcon: Icon(Icons.person),
+                            prefixIconColor: Colors.deepPurple,
+                            border: OutlineInputBorder(),
+                            labelText: "Username",
+                            labelStyle: TextStyle(color: Colors.deepPurple),
+                            errorStyle: TextStyle(color: Colors.red),
+                          ),
                         ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                            border: Border(
-                                top: BorderSide(color: Colors.cyan),
-                                bottom: BorderSide(color: Colors.cyan))),
-                        child: TextFormField(
+                        SizedBox(height: 20),
+                        TextFormField(
                           obscureText: true,
                           validator: (value) {
                             if (value!.isEmpty) {
@@ -90,79 +74,102 @@ class LoginScreen extends StatelessWidget {
                           },
                           controller: _passwordController,
                           decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Password",
-                              hintStyle: TextStyle(color: Colors.cyan)),
+                            prefixIcon: Icon(Icons.lock),
+                            prefixIconColor: Colors.deepPurple,
+                            border: OutlineInputBorder(),
+                            labelText: "Password",
+                            labelStyle: TextStyle(color: Colors.deepPurple),
+                            errorStyle: TextStyle(color: Colors.red),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-              Container(
-                  height: 50,
-                  margin: const EdgeInsets.fromLTRB(45, 0, 45, 0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient:
-                          const LinearGradient(colors: [Colors.cyan, Colors.blue])),
-                  child: InkWell(
-                    onTap: () async {
-                      try {
-                        if (_formKey.currentState!.validate()) {
-                          Authorization.username = _usernameController.text;
-                          Authorization.password = _passwordController.text;
-                          await _userProvider.login();
-                          // ignore: use_build_context_synchronously
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                                  title: const Text("Login success"),
-                                  content:
-                                      const Text("Success login"),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text("Ok"),
-                                      onPressed: () => Navigator.pop(context),
-                                    )
-                                  ],
-                                ));
-                        //   Navigator.popAndPushNamed(
-                        //       context, test.routeName);
-                        }
-                      } on Exception {
-                        // ignore: use_build_context_synchronously
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                                  title: const Text("Login failed"),
-                                  content:
-                                      const Text("Invalid username and/or password"),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text("Ok"),
-                                      onPressed: () => Navigator.pop(context),
-                                    )
-                                  ],
-                                ));
-                      }
-                    },
-                    child: const Center(child: Text("Login")),
-                  )),
-              const SizedBox(height: 20),
-              Center(
-                  child: InkWell(
-                child: const Center(
-                    child: Text(
-                  "You are not registered? Click here!",
-                  style: TextStyle(color: Colors.cyan),
-                )),
-                onTap: () {
-                  // Navigator.pushNamed(context, RegistrationScreen.routeName);
+            ),
+            Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+                gradient: LinearGradient(
+                  colors: [Colors.red, Colors.deepPurple],
+                ),
+              ),
+              child: InkWell(
+                onTap: () async {
+                  try {
+                    if (_formKey.currentState!.validate()) {
+                      Authorization.username = _usernameController.text;
+                      Authorization.password = _passwordController.text;
+                      await _userProvider.login();
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text("Login success"),
+                          content: const Text("Success login"),
+                          actions: [
+                            TextButton(
+                              child: const Text("Ok"),
+                              onPressed: () => Navigator.pop(context),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                  } on Exception {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text("Login failed"),
+                        content: const Text("Invalid username and/or password"),
+                        actions: [
+                          TextButton(
+                            child: const Text("Ok"),
+                            onPressed: () => Navigator.pop(context),
+                          )
+                        ],
+                      ),
+                    );
+                  }
                 },
-              )),
-            ],
-          ),
-        ));
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Navigate to registration screen
+              },
+              child: Text(
+                "Not Registered? Click here!",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
